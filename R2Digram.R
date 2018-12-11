50b51c3a02e39ae4341366b0d8d6a5fb852ccee6
Anne Lyngholm <als@sund.ku.dk>
2018-12-11 12:13:00 +0100

    Update R2Digram.R

--- R2Digram.R
+++ R2Digram.R
@@ -1,127 +1,27 @@
 # Auhtor: Anne Lyngholm Sørensen
 # Description: Create files for IR analysis in Digram
 
-library(data.table)
-library(ggplot2)
-library(eRm)
-library(psychotools)
-
-source("~/Documents/Karl/DISABKIDS/code/prepareDisab.R")
-
-dC11 <- data.frame(fullChild[,c("ID", "treat", "org", "gAge", "gHbA1c", "Gender",
-                                "X_1_G_CH", "X_2_G_CH", "X_3_G_CH",
-                                "X_4_G_CH", "X_5_G_CH", "X_6_G_CH")])
-dC12 <- data.frame(fullChild[,c("ID", "treat", "org", "gAge", "gHbA1c", "Gender",
-                                "X_1_G_CH", "X_2_G_CH", "X_3_G_CH",
-                                "X_4_G_CH", "X_5_G_CH", "X_6_G_CH")])
-
-data <- dC11
-idvar <- "ID"
-exo <- c("treat", "org", "gAge", "gHbA1c", "Gender")
-items <- c("X_1_G_CH", "X_2_G_CH", "X_3_G_CH",
-           "X_4_G_CH", "X_5_G_CH", "X_6_G_CH")
-
-
-RDigram(dC11, idvar = "ID", items = c("X_1_G_CH", "X_2_G_CH", "X_3_G_CH",
-                                      "X_4_G_CH", "X_5_G_CH", "X_6_G_CH"), exo = 
-          c("treat", "org", "gAge", "gHbA1c", "Gender"), folder = "D:\\GRM3\\", 
-        OS = "Windows")
-
-RDigram <- function(data, idvar, items, exo, folder, model, 
-                    personpar = FALSE, reduce = FALSE, LD = FALSE, DIF = FALSE, 
-                    GlobalTests = FALSE, Itemfits = FALSE, ARI = FALSE, 
-                    name = "DIGRAM", deletetemp = TRUE, commandfile = TRUE, OS = "Linux"){
+RDigram <- function(data, idvar, items, exo, folder, 
+                    name = "DIGRAM", OS = "Linux"){
   # return three files (.imp, .vmp, .exe) for creating an analysis in DIGRAM when
   # data is initially prepared in R
   #
   # args: 
   #   data: a data.frame containing the repsonses to the questionaire (items), id variable and exo. variables
-  #   idvar: name of the id variable from data
-  #   items: the names of the items from data
-  #   
+  #   idvar: name of the id variable from data (a character element)
+  #   items: the names of the items from data (character vector)
+  #   exo: the names of the exo. variables (character vector)
+  #   folder: Destination folder
+  #   name: name of the files (default: "DIGRAM")
+  #   OS: Operative System (options: "Windows" or "Linux", default: "Linux")
+  #
   # returns:
-  #   three files in a specified folder
+  #   nothing to the R console - but locally three files (.imp, .imv and .csv) in a specified folder
   
-  if(class(data) != "data.frame"){
-    stop("data must be a data.frame")
-  }
-  
-  if(OS == "Windows"){
-    delim <- "\n"
-  } else {
-    delim <- "\r\n"
-  }
-  
-  # create and export the .imp file (contains folder path, name of program (default is DIGRAM),
-  # and the command for opening)
-  outimp <- paste(folder, delim, name, delim, "-", delim, name, ".cmd", sep = "")
-  destpathimp <- paste(folder, "/", name, ".imp", sep = "")
-  write(outimp, destpathimp)
-  
-  # create and export the .imv file (contains the names of the items, names of each item level, 
-  # name of the exo var and the names of each level)
-  ncolI <- dim(data[,items])[2]
-  ncolE <- dim(data[,exo])[2]
-  
-  # item category range
-  rangeItems <- sapply(1:ncolI, function(x){
-    return(unique(data[,items][,x]))
-  })
-  
-  minI <- min(unlist(rangeItems), na.rm = TRUE)
-  maxI <- max(unlist(rangeItems), na.rm = TRUE)
-  rowsItems <- paste(minI:maxI, ",", numbers2words(minI:maxI), sep = "")
-  
-  # levels exo. variables
-  ExoMatrix <- sapply(1:ncolE, function(x){
-    as.numeric(factor(data[,exo][[x]]))
-  })
-  rangeExo <- sapply(1:ncolE, function(x){
-    return(sort(unique(ExoMatrix[,x])))
-  })
-  rowsExo <- sapply(1:ncolE, function(x){
-    paste(unlist(rangeExo[[x]]), ",", numbers2words(unlist(rangeExo[[x]])), sep = "")
-  })
-  
-  lrow <- letters[1:(ncolI+ncolE)]
-  namerow <- c(items, exo)
-  
-  outimv <- paste(
-    paste(
-      paste(lrow[1:ncolI], namerow[1:ncolI], paste(rowsItems, collapse = ","), sep = ","),
-      delim, sep =""), 
-    collapse = "")
-  outimv <- paste(outimv, paste(paste(lrow[(ncolI+1):(ncolI+ncolE)], namerow[(ncolI+1):(ncolI+ncolE)],
-        sapply( rowsExo, paste0, collapse=","), sep = ","), delim, sep= "", collapse = ""),
-        sep = "", collapse = "")
-  
-  if(OS == "Windows"){
-    outimv <- gsub('.{1}$', '', outimv)
-  } else {
-    outimv <- gsub('.{2}$', '', outimv)
-  }
-  
-  destpathimv <- paste(folder, "/", name, ".imv", sep = "")
-  write(outimv, destpathimv)
-  
-  # create and export the .csv file (contains the needed data)
-  csvdata <- data[,colnames(data) %in% c(idvar, items)]
-  csvdata$ID <- 1:dim(data)[1]
-  csvdata <- cbind(csvdata, ExoMatrix)
-  #colnames(csvdata) <- c("ID", items, exo)
-  csvdata <- rbind(c("ID", items, exo), csvdata)
-  
-  df_args <- c(csvdata, sep=",")
-  csvdata <- do.call(paste, df_args)
-  csvdata <- gsub("NA","", csvdata, perl = T)
-  
-  csvdata <- paste(csvdata, collapse = delim)
-  
-  destpathcsv <- paste(folder, "/", name, ".csv", sep = "")
-  write(csvdata, destpathcsv)
-  }
-
+  # helper function for number to written number:
 numbers2words <- function(x){
+  # needed function for number to written number
+  # originally by:
   ## Function by John Fox found here: 
   ## http://tolstoy.newcastle.edu.au/R/help/05/04/2715.html
   ## Tweaks by AJH to add commas and "and"
@@ -172,3 +72,89 @@ numbers2words <- function(x){
   helper(x)
 }
 
+  ### R to DIGRAM CODE ###
+  
+  # data must be a data frame
+  if(class(data) != "data.frame"){
+    stop("data must be a data.frame")
+  }
+  
+  # create delimiting argument based on OS
+  if(OS == "Windows"){
+    delim <- "\n"
+  } else {
+    delim <- "\r\n"
+  }
+  
+  # create and export the .imp file (contains folder path, name of program (default is DIGRAM),
+  # and the command for opening)
+  outimp <- paste(folder, delim, name, delim, "-", delim, name, ".cmd", sep = "")
+  destpathimp <- paste(folder, "/", name, ".imp", sep = "")
+  write(outimp, destpathimp)
+  
+  # create and export the .imv file (contains the names of the items, names of each item level, 
+  # name of the exo var and the names of each level)
+  ncolI <- dim(data[,items])[2]
+  ncolE <- dim(data[,exo])[2]
+  
+  # item category range
+  rangeItems <- sapply(1:ncolI, function(x){
+    return(unique(data[,items][,x]))
+  })
+  minI <- min(unlist(rangeItems), na.rm = TRUE)
+  maxI <- max(unlist(rangeItems), na.rm = TRUE)
+  
+  # create the fill for .imv file for items
+  rowsItems <- paste(minI:maxI, ",", numbers2words(minI:maxI), sep = "")
+  
+  # levels exo. variables
+  ExoMatrix <- sapply(1:ncolE, function(x){
+    as.numeric(factor(data[,exo][[x]]))
+  })
+  rangeExo <- sapply(1:ncolE, function(x){
+    return(sort(unique(ExoMatrix[,x])))
+  })
+  rowsExo <- sapply(1:ncolE, function(x){
+    paste(unlist(rangeExo[[x]]), ",", numbers2words(unlist(rangeExo[[x]])), sep = "")
+  })
+  
+  lrow <- letters[1:(ncolI+ncolE)]
+  namerow <- c(items, exo)
+  
+  # create the fill for the .imv for exo. variables
+  outimv <- paste(
+    paste(
+      paste(lrow[1:ncolI], namerow[1:ncolI], paste(rowsItems, collapse = ","), sep = ","),
+      delim, sep =""), 
+    collapse = "")
+  outimv <- paste(outimv, paste(paste(lrow[(ncolI+1):(ncolI+ncolE)], namerow[(ncolI+1):(ncolI+ncolE)],
+        sapply( rowsExo, paste0, collapse=","), sep = ","), delim, sep= "", collapse = ""),
+        sep = "", collapse = "")
+  
+  # remove the last line breaker
+  if(OS == "Windows"){
+    outimv <- gsub('.{1}$', '', outimv)
+  } else {
+    outimv <- gsub('.{2}$', '', outimv)
+  }
+  
+  destpathimv <- paste(folder, "/", name, ".imv", sep = "")
+  write(outimv, destpathimv)
+  
+  # create and export the .csv file (contains the needed data)
+  # truncate the data set
+  csvdata <- data[,colnames(data) %in% c(idvar, items)]
+  csvdata$ID <- 1:dim(data)[1]
+  csvdata <- cbind(csvdata, ExoMatrix)
+  
+  # store the file as one long character string
+  csvdata <- rbind(c("ID", items, exo), csvdata)
+  df_args <- c(csvdata, sep=",")
+  csvdata <- do.call(paste, df_args)
+  csvdata <- gsub("NA","", csvdata, perl = T)
+  csvdata <- paste(csvdata, collapse = delim)
+  
+  # writ
+  destpathcsv <- paste(folder, "/", name, ".csv", sep = "")
+  write(csvdata, destpathcsv)
+  }
